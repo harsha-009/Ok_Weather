0feb7fc32a6f7269b845e8d31cd77f0138a242a4
Sri Harsha Varanasi <varanasiharsha09@gmail.com>
2020-06-29 17:08:49 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_share_me/FlutterShareMePlugin.h>)
+#import <flutter_share_me/FlutterShareMePlugin.h>
+#else
+@import flutter_share_me;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterShareMePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterShareMePlugin"]];
+}
+
+@end
